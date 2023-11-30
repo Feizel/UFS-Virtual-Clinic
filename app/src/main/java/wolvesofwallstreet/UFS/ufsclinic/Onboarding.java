499b7f626beff832f507caed1d52529007273eaf
Feizel <feizel.maduna@gmail.com>
2023-12-01 00:11:33 +0200

    Onboarding logic code: working on the different screens text changing with each image.

--- app/src/main/java/wolvesofwallstreet/UFS/ufsclinic/Onboarding.java
+++ app/src/main/java/wolvesofwallstreet/UFS/ufsclinic/Onboarding.java
@@ -8,11 +8,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageSwitcher;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.ViewSwitcher;
 
 public class Onboarding extends AppCompatActivity {
     private ImageSwitcher imgSwitcher;
+    private ImageView dot1, dot2, dot3;
+    private LinearLayout dotsLayout;
     private Button btnSkipTour, btnNext;
     private static final int[] onboardingImages = {
             R.drawable.onb1,
@@ -39,6 +42,13 @@ public class Onboarding extends AppCompatActivity {
         imgSwitcher = findViewById(R.id.imgSwitcherONB);
         btnSkipTour = findViewById(R.id.btnSkipTour);
         btnNext = findViewById(R.id.btnNext);
+
+        dot1 = findViewById(R.id.dot1);
+        dot2 = findViewById(R.id.dot2);
+        dot3 = findViewById(R.id.dot3);
+        dotsLayout = findViewById(R.id.dotsLayout);
+
+        updateDots();
 
         imgSwitcher.setFactory(new ViewSwitcher.ViewFactory() {
             @Override
@@ -67,6 +77,26 @@ public class Onboarding extends AppCompatActivity {
         });
     }
 
+    private void updateDots() {
+        switch (currentIndex) {
+            case 0:
+                dot1.setImageResource(R.drawable.dot_active);
+                dot2.setImageResource(R.drawable.dot_inactive);
+                dot3.setImageResource(R.drawable.dot_inactive);
+                break;
+            case 1:
+                dot1.setImageResource(R.drawable.dot_inactive);
+                dot2.setImageResource(R.drawable.dot_active);
+                dot3.setImageResource(R.drawable.dot_inactive);
+                break;
+            case 2:
+                dot1.setImageResource(R.drawable.dot_inactive);
+                dot2.setImageResource(R.drawable.dot_inactive);
+                dot3.setImageResource(R.drawable.dot_active);
+                break;
+            default:
+        }
+    }
     private void updateImageAndText() {
         imgSwitcher.setImageResource(onboardingImages[currentIndex]);
 
--- app/src/main/java/wolvesofwallstreet/UFS/ufsclinic/Splash.java
+++ app/src/main/java/wolvesofwallstreet/UFS/ufsclinic/Splash.java
@@ -8,14 +8,13 @@ import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
-import android.view.View;
 
 
 public class Splash extends Activity
 {
     private ImageView imgLogo;
     private Animation log_in;
-    private static final int TIME = 2000;
+    private static final int TIME = 3000;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -25,7 +24,7 @@ public class Splash extends Activity
 
         imgLogo =  findViewById(R.id.imgLogo);
 
-        log_in = AnimationUtils.loadAnimation(this, R.anim.log_in);
+        log_in = AnimationUtils.loadAnimation(this, R.anim.logo_in);
 
         imgLogo.setAnimation(log_in);
 
