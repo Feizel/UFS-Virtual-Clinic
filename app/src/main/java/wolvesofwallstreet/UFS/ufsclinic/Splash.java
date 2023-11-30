72c2441e20b0b07bd8c59df9ca9cf54c6459431d
Feizel <feizel.maduna@gmail.com>
2023-12-01 00:14:22 +0200

    Onboarding logic code: working on the different screens text changing with each image.

--- app/src/main/java/wolvesofwallstreet/UFS/ufsclinic/Splash.java
+++ app/src/main/java/wolvesofwallstreet/UFS/ufsclinic/Splash.java
@@ -9,34 +9,23 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
+public class Splash extends Activity {
+    private static final int TIME = 2000;
 
-public class Splash extends Activity
-{
-    private ImageView imgLogo;
-    private Animation log_in;
-    private static final int TIME = 3000;
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
 
-        imgLogo =  findViewById(R.id.imgLogo);
+        ImageView imgLogo = findViewById(R.id.imgLogo);
+        Animation logInAnimation = AnimationUtils.loadAnimation(this, R.anim.logo_in);
+        imgLogo.setAnimation(logInAnimation);
 
-        log_in = AnimationUtils.loadAnimation(this, R.anim.logo_in);
-
-        imgLogo.setAnimation(log_in);
-
-        new Handler().postDelayed(new Runnable()
-        {
-            @Override
-            public void run()
-            {
-                Intent intent = new Intent(Splash.this, Onboarding.class);
-                startActivity(intent);
-                finish();
-            }
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(Splash.this, Onboarding.class);
+            startActivity(intent);
+            finish();
         }, TIME);
     }
 }
